12b8a86e691f654b7797abb4133f07c7ea0fddf0
Tin Ho Trong <hotinbk@gmail.com>
2020-06-12 12:30:47 +0700

    Xong buoc 2: hardcoded

--- Bookstore/thuvienbk/views.py
+++ Bookstore/thuvienbk/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import redirect
 from django.http import JsonResponse
 from web3 import Web3
 import json
+# pprint de debug
+from pprint import pprint
 
 
 from django.shortcuts import get_object_or_404, render
@@ -61,18 +63,28 @@ def audit_page_view(request, isbn):
     print(context)
     return render(request, 'pages/Audit-verHtml/audit.html', context)
 
-
 def audit_result(request):
     if request.method == 'POST':
         print("Im listening...")
         
         # data mẫu thôi, tự mà kéo data về bằng web3
-        data = {
-            'name': 'Vitor',
-            'location': 'Finland',
-            'is_active': True,
-            'count': 28
-        }
+        # data = {
+        #     'name': 'Vitor',
+        #     'location': 'Finland',
+        #     'is_active': True,
+        #     'count': 28
+        # }
+
+        # Owner address
+        owner = "0xAb076e2f130e7Da2985B99a2b5Ce5af6cc34f82e"
+
+        # Get user info
+        user_address = PaymentMethod.objects.get(user_id= request.user.id).wallet_address
+        user_pkey = PaymentMethod.objects.get(user_id= request.user.id).private_key
+
+        # Get book price
+        book_price = Book.objects.get(ISBN=request.POST['isbn']).price
+        book_isbn = Book.objects.get(ISBN=request.POST['isbn']).ISBN
 
         # Implement web3
         # Connecting to web3 + Initiating web3
@@ -80,33 +92,36 @@ def audit_result(request):
         web3 = Web3(Web3.HTTPProvider(url))
 
         # Contract address + abi
-        address = web3.toChecksumAddress("0xE498c05E9589c126f9A66A7841AF19c5824d0193")
+        contract_address = web3.toChecksumAddress("0xE498c05E9589c126f9A66A7841AF19c5824d0193")
         abi = json.loads('[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"constant":true,"inputs":[{"internalType":"address","name":"_user","type":"address"},{"internalType":"uint256","name":"_token","type":"uint256"}],"name":"eachTokenCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address payable","name":"_from","type":"address"},{"internalType":"address payable","name":"_to","type":"address"},{"internalType":"uint256","name":"ether_amount","type":"uint256"},{"internalType":"uint256","name":"token","type":"uint256"}],"name":"implementTransaction","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"isOwner","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"internalType":"address payable","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"_user","type":"address"}],"name":"ownerToken","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address payable","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]')
 
         #Creating contract instance
-        contract = web3.eth.contract(address, abi=abi)
+        contract = web3.eth.contract(contract_address, abi=abi)
 
         #Call a simple blockNumer getter
         #print(web3.eth.blockNumber)
 
-        # Request view owned token list
-        result = contract.functions.ownerToken('0x7De8d441fBb7a6868D3ce81eE26d1Fc0868E8761').call()
-        print("Token list: ")
-        print(result)
 
         # Prepare Implement transaction
-        nonce = web3.eth.getTransactionCount('0x7De8d441fBb7a6868D3ce81eE26d1Fc0868E8761')
-        tx = contract.functions.implementTransaction('0x7De8d441fBb7a6868D3ce81eE26d1Fc0868E8761', "0xAb076e2f130e7Da2985B99a2b5Ce5af6cc34f82e", web3.toWei('0.1', "ether"), 11111111).buildTransaction({
-            'from': '0x7De8d441fBb7a6868D3ce81eE26d1Fc0868E8761',
-            'value': web3.toWei(0.1, "ether"),
+        nonce = web3.eth.getTransactionCount(user_address)
+        tx = contract.functions.implementTransaction(user_address, owner, web3.toWei(book_price, "ether"), book_isbn).buildTransaction({
+            'from': user_address,
+            'value': web3.toWei(book_price, "ether"),
             'nonce': nonce
         })
 
-        signed_tx = web3.eth.account.signTransaction(tx, '6B4C896CAEB9839A433F0BADBBB2FA28D7B2691E268DFD577C55D9B6D606EC7D')
+        signed_tx = web3.eth.account.signTransaction(tx, user_pkey)
         tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
         tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
         print("Transaction receipt: ")
         print(tx_receipt)
+
+
+        # Request view owned token list
+        result = contract.functions.ownerToken(user_address).call()
+        print("Token list: ")
+        print(result)
+        
         # Phải làm kiểu này để data có dạng json
         data = {}
         data['result'] = result
